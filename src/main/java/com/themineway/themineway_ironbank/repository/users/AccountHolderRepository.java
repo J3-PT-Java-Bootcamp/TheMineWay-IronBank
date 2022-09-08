2938912ad73072bb011dc27babdac278deb2bad8
Joel Campos <joelcamposoliva9000@gmail.com>
2022-09-08 19:42:28 +0200

    Remove IUserRepository

--- src/main/java/com/themineway/themineway_ironbank/repository/users/AccountHolderRepository.java
+++ src/main/java/com/themineway/themineway_ironbank/repository/users/AccountHolderRepository.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AccountHolderRepository implements IUserRepository {
+public class AccountHolderRepository {
 
     @Autowired
     private UserRepository userRepository;
--- src/main/java/com/themineway/themineway_ironbank/repository/users/AdminRepository.java
+++ src/main/java/com/themineway/themineway_ironbank/repository/users/AdminRepository.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AdminRepository implements IUserRepository {
+public class AdminRepository {
 
     @Autowired
     private UserRepository userRepository;
