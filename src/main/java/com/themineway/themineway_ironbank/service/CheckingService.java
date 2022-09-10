6b046b7feffe2be6df7114ce66ac12a1edcf5363
Joel Campos <joelcamposoliva9000@gmail.com>
2022-09-10 17:11:08 +0200

    All account creations

--- src/main/java/com/themineway/themineway_ironbank/service/CheckingService.java
+++ src/main/java/com/themineway/themineway_ironbank/service/CheckingService.java
@@ -1,10 +1,16 @@
 package com.themineway.themineway_ironbank.service;
 
 import com.themineway.themineway_ironbank.model.accounts.Checking;
+import com.themineway.themineway_ironbank.model.accounts.StudentChecking;
 import com.themineway.themineway_ironbank.repository.accounts.CheckingRepository;
+import com.themineway.themineway_ironbank.repository.accounts.StudentCheckingRepository;
 import com.themineway.themineway_ironbank.repository.users.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
 
 @Component
 public class CheckingService {
@@ -13,11 +19,31 @@ public class CheckingService {
     CheckingRepository checkingRepository;
 
     @Autowired
+    StudentCheckingRepository studentCheckingRepository;
+
+    @Autowired
     UserRepository userRepository;
 
     public void createChecking(Checking checking) {
-        final var primaryOwner = userRepository.findById(checking.getPrimaryOwner().getId());
-        checkingRepository.save(checking);
+        final var primaryOwnerFetch = userRepository.findById(checking.getPrimaryOwner().getId());
+        if(primaryOwnerFetch.isEmpty()) return; // TODO: throw exception
+
+        final var primaryOwner = primaryOwnerFetch.get();
+        final var age = Period.between(primaryOwner.getBirthDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), LocalDate.now()).getYears();
+
+        if(age < 24) {
+            final var studentChecking = new StudentChecking();
+
+            studentChecking.setAccountStatus(checking.getAccountStatus());
+            studentChecking.setBalance(checking.getBalance());
+            studentChecking.setPenaltyFee(checking.getPenaltyFee());
+            studentChecking.setSecretKey(checking.getSecretKey());
+            studentChecking.setPrimaryOwner(checking.getPrimaryOwner());
+
+            studentCheckingRepository.save(studentChecking);
+        } else {
+            checkingRepository.save(checking);
+        }
     }
 
 }
