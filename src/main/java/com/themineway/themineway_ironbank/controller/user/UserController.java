d0376db85dfda645113a1e10d5390f12880dd388
Joel Campos <joelcamposoliva9000@gmail.com>
2022-09-16 15:35:02 +0200

    Create users

--- src/main/java/com/themineway/themineway_ironbank/controller/user/UserController.java
+++ src/main/java/com/themineway/themineway_ironbank/controller/user/UserController.java
@@ -1,6 +1,8 @@
 package com.themineway.themineway_ironbank.controller.user;
 
+import com.themineway.themineway_ironbank.dto.users.CreateAccountHolderDTO;
 import com.themineway.themineway_ironbank.dto.users.CreateAdminDTO;
+import com.themineway.themineway_ironbank.dto.users.CreateThirdPartyDTO;
 import com.themineway.themineway_ironbank.model.users.UserType;
 import com.themineway.themineway_ironbank.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +19,24 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @PostMapping("account-holder")
+    public void createAccountHolder(
+        @Validated @RequestBody CreateAccountHolderDTO createAccountHolderDTO
+    ) {
+        userService.createUser(createAccountHolderDTO.toUser(), UserType.ACCOUNT_HOLDER);
+    }
+
     @PostMapping("admin")
     public void createAdmin(
         @Validated @RequestBody CreateAdminDTO createAdminDto
     ) {
         userService.createUser(createAdminDto.toUser(), UserType.ADMIN);
+    }
+
+    @PostMapping("third-party")
+    public void createAccountHolder(
+        @Validated @RequestBody CreateThirdPartyDTO createThirdPartyDTO
+    ) {
+        userService.createUser(createThirdPartyDTO.toUser(), UserType.THIRD_PARTY);
     }
 }
