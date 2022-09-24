a1a355160ced04a1560d65763713dbd4ff90d874
Joel Campos <joelcamposoliva9000@gmail.com>
2022-09-24 09:07:59 +0200

    CORS

--- src/main/java/com/themineway/themineway_ironbank/controller/user/UserController.java
+++ src/main/java/com/themineway/themineway_ironbank/controller/user/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(path = "user")
 public class UserController {
